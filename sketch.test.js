b343537fb36972ea6132b5e4bdc6f82c3ab660af
augustoecji <augustoecji@unisabana.edu.co>
2020-04-17 22:20:32 -0500

    archivo nice para presentar

--- sketch.test.js
+++ sketch.test.js
@@ -1,5 +1,9 @@
-const sum = require('./sketch');
+const {sum, prod} = require('./sketch');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('prod calculates 5 * 100 = 500', () => {
+  expect(prod(5, 100)).toBe(500);
 });
